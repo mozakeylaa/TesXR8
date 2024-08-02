555846d087aea3c633fbd01ba62f8915a809856d
mozakeylaa <moza_keyla_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 14:58:00 +0700

    Add files via upload

---
+++ VSCODE/Hello.java
@@ -0,0 +1,19 @@
+public class Hello{
+    public static void main(String[] args) {
+
+        String nama= "Moza";
+        String asalKota= "Malang";
+        int usia= 16;
+        double beratBadan= 51.00;
+        float tinggiBadan= 155.0f;
+        char jenisKelamin= 'P';
+        boolean status= true;
+
+        System.out.println("Nama: "+nama);
+        System.out.println("Usia: "+usia);
+        System.out.println("Berat Badan: "+beratBadan);
+        System.out.println("Tinggi Badan: "+tinggiBadan);
+        System.out.println("Jenis Kelamin: "+jenisKelamin);
+        System.out.println("Status: "+status);
+    }
+}
