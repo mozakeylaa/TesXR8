c01d66a184ae8d0b16260633e40568074293c1fb
mozakeylaa <moza_keyla_33rpl@student.smktelkom-mlg.sch.id>
2024-09-13 12:41:00 +0700

    Add files via upload

---
+++ DVD.java
@@ -0,0 +1,42 @@
+public class DVD extends Product{
+    int length;
+    String rating;
+    String studio;
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getRating() {
+        return this.rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getStudio() {
+        return this.studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public DVD(){
+        super();
+        length = 0;
+        rating = "";
+        studio = "";   
+    }
+    public DVD(int number, String name, int quantity, double price, int length, String rating, String studio){
+        super(number, name, quantity, price);
+        this.length = length;
+        this.rating = rating;
+        this.studio = studio; 
+    }
+}
